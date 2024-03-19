92211d1aa09473238125ed220d60e69b3da8fd0e
Egroroxy <u.nishonxodjaev@newuu.uz>
2024-03-19 21:38:21 +0500

    i wrote Hello world

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <math.h>
+
+ cout << "PROBLEM 1\n";
+    int intNumber = 23;
+    float floatNumber = 3.14;
+    double doubleNumber = 45.1234;
+    bool boolean = true;
+    char charName = 'A';
+    cout << "Value of Integer is " << intNumber <<  ". Size is " << sizeof(intNumber) <<endl;
+    cout << "Value of Float is " << intNumber <<  ". Size is " << sizeof(float) <<endl;
+    cout << " Value of Double is " << intNumber << ".Size is " << sizeof (double ) << endl;
+    cout << "Value of Bool is " << intNumber << ".Size is " << sizeof (bool) << endl;
+    cout << "Value of Char is " << intNumber << ".Size is " <<sizeof(char) << endl;
+    cout << "PROBLEM 2\n";
+    cout << "Ulugbek Nishonxodjaev" << endl;
+    cout << "230310 " << endl;
+    cout << "Idea 22 - Neon" << endl;
+    cout << "Avengers\n";
