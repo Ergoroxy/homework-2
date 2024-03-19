23c03b3b585000a11344fdc45f64e33178b2129a
Egroroxy <u.nishonxodjaev@newuu.uz>
2024-03-19 21:38:21 +0500

    i solved 1 problem

---
+++ main.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+ using namespace std;
+int main (){
+cout << "Problem 3\n";
+    int num1,num2;
+    float avg , product;
+    cout << "Enter the first number:";
+    cin >> num1;
+    cout << "Enter the second number:";
+    cin >> num2;
+    avg = num1 + num2/2;
+    product = num1 * num2;
+    cout << "The avg of two numbers:" << avg << endl;
+    cout << " The product of two numbers :" << product << endl;
+    return 0; }
+
+
